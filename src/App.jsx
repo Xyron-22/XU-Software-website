2bb41192c291a5d2494bc7642f1b153195318fbf
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2024-01-30 12:32:53 +0800

    development stage 2, added hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ function App() {
       <div className='text-white flex flex-col items-center min-h-[80vh]'>
         <section className="bg-headerBackground bg-cover bg-no-repeat bg-center w-full min-h-[90vh] flex items-center p-4">
           <div className='leading-none md:ml-[10%] flex flex-col flex-wrap break-all xs:break-normal'>
-            <p className='text-2xl font-bold'>We Provide</p>
-            <h1 className='text-[400%] font-black text-[#535bf2] tracking-tighter'>SOFTWARE PRODUCTS</h1>
-            <p className='text-2xl font-bold'>And</p>
-            <h1 className='text-[400%] font-black text-[#535bf2] tracking-tighter'>SERVICES.</h1>
+            <p className='text-xl sm:text-2xl font-bold'>We Provide</p>
+            <h1 className='text-[300%] sm:text-[400%] font-black text-[#535bf2] tracking-tighter'>SOFTWARE PRODUCTS</h1>
+            <p className='text-xl sm:text-2xl font-bold'>And</p>
+            <h1 className='text-[300%] sm:text-[400%] font-black text-[#535bf2] tracking-tighter'>SERVICES.</h1>
             <p className='md:w-[50%] font-semibold leading-6'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nostrum velit autem amet quasi molestiae, esse similique odio magni natus minima praesentium modi, dolore ullam voluptatem veritatis maxime repudiandae suscipit.</p>
-            <div className='font-bold text-2xl'>
+            <div className='font-bold text-xl sm:text-2xl'>
               <button className='rounded p-4 m-4 ml-0 mb-0 bg-white text-[#535bf2]'>Get Started</button>
               <button className='rounded p-4 m-4 ml-0 mb-0 bg-[#535bf2] text-white'>Contact Us</button>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,23 +5,34 @@ const Header = () => {
   const [toggleMenu, setToggleMenu] = useState(false)
 
   return (
-    <nav className='min-h-[10vh] flex mx-[10%]'>
-        <div className='w-[50%] my-auto'>
-          <h1 className='text-xl md:text-[2.5rem] font-bold italic'>XU Software</h1>
+    <>
+    <nav className='min-h-[10vh] flex justify-evenly mx-auto bg-black sticky top-0'>
+        <div className='w-[40%] my-auto text-center xl:text-start'>
+          <h1 className='text-2xl md:text-[2.5rem] font-bold italic text-white'>XU Software</h1>
         </div>
-        <div className='md:hidden w-[50%] flex justify-end items-center'>
-          <RiMenuFoldLine onClick={() => setToggleMenu(!toggleMenu)} className='text-2xl'/>
+        <div className='lg:hidden w-[40%] flex justify-end items-center'>
+          <RiMenuFoldLine onClick={() => setToggleMenu(!toggleMenu)} className='text-4xl md:text-5xl text-white'/>
         </div>
-        <div className={`w-[50%] my-auto hidden md:inline ${toggleMenu ? "inline" : "hidden"}`}>
-          <ul className='w-full flex justify-end font-bold'>
-            <li className='m-5 p-2 rounded'><a>About</a></li>
-            <li className='m-5 p-2 rounded'><a>Projects</a></li>
-            <li className='m-5 p-2 rounded'><a>Services</a></li>
-            <li className='m-5 p-2 rounded'><a>Developers</a></li>
-            <li className='m-5 bg-gradient-to-r hover:from-[#7d12ff] hover:to-[#ab20fd] p-2 rounded'><a>Contact</a></li>
+        <div className={`w-full lg:w-auto my-auto lg:inline left-0 top-[10vh] lg:top-0 absolute lg:relative ${toggleMenu ? "inline" : "hidden"}`}>
+          <ul className={`flex lg:justify-end lg:flex-row font-bold text-white text-xl cursor-pointer flex-col`}>
+            <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>About</a></li>
+            <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Projects</a></li>
+            <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Products</a></li>
+            <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Services</a></li>
+            <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Developers</a></li>
+            <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Contact</a></li>
           </ul>
         </div>
     </nav>
+    {/* <section className={`bg-black flex flex-col absolute lg:hidden ${toggleMenu ? "inline text-white w-full" : "hidden"}`}>
+      <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>About</a></li>
+      <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Projects</a></li>
+      <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Products</a></li>
+      <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Services</a></li>
+      <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Developers</a></li>
+      <li className='m-3 p-1 rounded hover:scale-110 hover:text-[#535bf2]'><a>Contact</a></li>
+    </section> */}
+    </>
   )
 }
 
