7dd9902689966c328584401cc63f2edc8401b5f4
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2024-08-03 16:30:37 +0800

    finalizing contents

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,26 @@ import {
 import sampleLogo from "./assets/sample-logo.jpg"
 import profilePicture from "./assets/profile.jpg"
 import {servicesData} from "./data/services-data"
-import { FaLinkedin, FaGithub, FaFacebook } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaFacebook, FaCheck, FaPhoneAlt } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
+import { FaSquareUpwork } from "react-icons/fa6";
+
 
 function App() {
 
   return (
     <>
       <div className='text-white flex flex-col items-center min-h-[80vh]'>
-        <section className="bg-headerBackground bg-cover bg-no-repeat bg-center w-full min-h-[90vh] flex items-center">
+        <section className="bg-headerBackground bg-cover bg-no-repeat bg-center w-full min-h-[90vh] flex items-center" id='home'>
           <div className='leading-none flex flex-col flex-wrap break-all xs:break-normal container mx-auto px-4'>
             <Typography variant='lead'>We Provide</Typography>
             <Typography variant='h1' className='text-blue font-black'>SOFTWARE PRODUCTS</Typography>
             <Typography variant='lead'>And</Typography>
             <Typography variant='h1' className='text-blue font-black'>SERVICES.</Typography>
-            <Typography className='md:w-[50%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nostrum velit autem amet quasi molestiae, esse similique odio magni natus minima praesentium modi, dolore ullam voluptatem veritatis maxime repudiandae suscipit.</Typography>
-            <div className='font-bold text-xl sm:text-2xl'>
-              <Button className='rounded p-4 m-4 ml-0 mb-0 bg-white text-[#535bf2]'>Get Started</Button>
-              <Button className='rounded p-4 m-4 ml-0 mb-0 bg-[#535bf2] text-white'>Contact Us</Button>
+            <Typography className='md:w-[50%]'>Dedicated to driving innovation and excellence in the software industry. With a focus on quality, efficiency, and customer satisfaction, we deliver robust and scalable software products that empower our clients to achieve their goals.</Typography>
+            <div className='font-bold text-xl sm:text-2xl mt-10'>
+              <a href='#services' className='p-3 m-4 ml-0 mb-0 bg-white text-[#535bf2] text-base'>Get Started</a>
+              <a href='#contact' className='p-3 m-4 ml-0 mb-0 bg-[#535bf2] text-white text-base'>Contact Us</a>
             </div>
           </div>
         </section>
@@ -43,43 +45,42 @@ function App() {
             </div>
             <div className='flex flex-row lg:flex-col gap-2 text-lightGray'>
               <div className='p-1 border border-lightGray text-5xl hover:text-white'> 
-                  <FaLinkedin className='hover:scale-110'></FaLinkedin>
+                  <a href='https://www.linkedin.com/in/xyron-uy-b5a915213/' target='_blank'><FaLinkedin className='hover:scale-110 hover:cursor-pointer'></FaLinkedin></a>
               </div>
-              <div className='p-1 border border-lightGray text-5xl'> 
-                <FaGithub></FaGithub>
+              <div className='p-1 border border-lightGray text-5xl hover:text-white'> 
+                  <a href='https://github.com/Xyron-22' target='_blank'><FaGithub className='hover:scale-110 hover:cursor-pointer'></FaGithub></a>
               </div>
-              <div className='p-1 border border-lightGray text-5xl'> 
-                <SiGmail></SiGmail>
+              <div className='p-1 border border-lightGray text-5xl hover:text-white'> 
+                  <a href='https://mail.google.com/mail/u/0/#inbox?compose=DmwnWsTPLdWXZDrvSCqBlMHqgTwfcwKDSPQtmpsdjFdBrrFCrRhSKHXTrmkFpgqLdnkRLqBSsmfG' target='_blank'><SiGmail className='hover:scale-110 hover:cursor-pointer'></SiGmail></a>
               </div>
-              <div className='p-1 border border-lightGray text-5xl'> 
-                <FaFacebook></FaFacebook>
+              <div className='p-1 border border-lightGray text-5xl hover:text-white'> 
+                  <a href='https://www.upwork.com/freelancers/~0166e6120ac6cdb97d' target='_blank'><FaSquareUpwork className='hover:scale-110 hover:cursor-pointer'></FaSquareUpwork></a>
               </div>
             </div>
             <div className='lg:w-[50%]'>
               <Typography variant='h2' className='mb-10'>About Me.</Typography>
-              <Typography variant='lead' className='text-lightColor mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</Typography>
-              <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia maiores corrupti quae qui sunt sapiente rerum maxime, voluptas, odio optio reiciendis eveniet blanditiis fuga pariatur temporibus! Eum reprehenderit harum minima.</Typography>
+              <Typography variant='lead' className='text-lightColor mb-5'>Hi I am Xyron Uy a Fullstack Developer.</Typography>
+              <Typography id='works'>Graduate of Bachelor of Science in Mechanical Engineering. I am a fullstack developer with expertise in Javascript, How I got into programming was I am looking for a skill where I can apply my analytical, logical, and problem solving abilities, while surfing the internet I found software development, Tried doing it, got hooked to it, and fell in love doing it since. Now I am looking for ways to connect my skill to other people so I can bring them value that benefits them.</Typography>
             </div>
           </div>
         </section>
-        <section className='w-full py-[2.5%] md:px-[10%] bg-geoBackground bg-center bg-no-repeat bg-cover bg-blend-darken'>
-          <div className='w-full gap-2 flex flex-col items-center container mx-auto px-4'>
-            <img src={sampleLogo} alt='company-image' className='w-[40%] md:w-[10%] rounded-full overflow-hidden'></img>
+        <section className='w-full py-5 bg-geoBackground bg-center bg-no-repeat bg-cover bg-blend-darken my-2'>
+          <div className='container mx-auto px-4 mb-5'>
+            <Typography variant='h4' className='mb-5 text-lightColor text-center'>We've Worked With.</Typography>
+            <div className='w-full gap-2 flex flex-col items-center'>
+              <img src={sampleLogo} alt='company-image' className='w-[40%] md:w-[10%] overflow-hidden'></img>
+            </div>
           </div>
         </section>
-        {/* <section className='w-full bg-medium py-[5%] text-white flex flex-col justify-center items-center'>
-          <h1 className='text-center text-2xl md:text-4xl font-bold'>Products</h1>
-          <p className='text-xl'>Coming soon...</p>
-        </section> */}
-        <section className='w-full py-[5%] bg-light'>
-          {/* <h1 className='text-center text-2xl md:text-4xl font-bold text-white'></h1> */}
+        <section className='w-full py-[5%] bg-light' id='services'>
           <div className='container mx-auto px-4'>
+            <Typography variant='h2' className='mb-5'>Services.</Typography>
             <Typography variant='h4' color='white' className='w-full md:w-96 font-light mb-10'>We design and develop software technology that drives growth to your business.</Typography>
             <div className='w-full h-full flex flex-wrap gap-2 xl:gap-10 items-center justify-center mb-10'>
               {servicesData.map(({imageSource, alt, cardHeader, cardBody}, index) => {
                 return (
-                  <Card key={index} className="mt-6 w-full sm:w-full md:w-96">
-                  <CardHeader color="blue-gray" className="relative h-80">
+                  <Card key={index} className="mt-6 w-full sm:w-full md:w-96 rounded-none bg-medium">
+                  <CardHeader color="blue-gray" className="relative h-80 rounded-none">
                     <img
                       className='object-cover object-bottom w-full h-full'
                       src={imageSource}
@@ -87,15 +88,15 @@ function App() {
                     />
                   </CardHeader>
                   <CardBody>
-                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                    <Typography variant="h5" className="mb-2 text-white">
                       {cardHeader}
                     </Typography>
-                    <Typography color='blue-gray'>
+                    <Typography className='text-lightColor'>
                       {cardBody}
                     </Typography>
                   </CardBody>
                   <CardFooter className="pt-0">
-                    <Button className='bg-blue' variant='gradient'>Read More</Button>
+                    <Button className='bg-blue rounded-none' variant='gradient'>Read More</Button>
                   </CardFooter>
                 </Card>
                 )
@@ -103,18 +104,45 @@ function App() {
             </div>
             <div className='w-full'>
               <Typography variant='h3' color='white' className='font-semibold'>Let us turn your ideas into high quality innovation</Typography>
-              <Typography variant='lead' className='font-light text-lightColor'>Provide us the details and we will get in touch with you to discuss the project.</Typography>
+              <Typography variant='lead' className='font-light text-lightColor mb-10'>Provide us the details and we will get in touch with you to discuss the project.</Typography>               
+              <div className='flex flex-col gap-5 break-words'>
+              <Typography variant='lead' className='flex gap-5'>
+                <FaCheck className='text-blue text-3xl min-w-10 mt-[.7]'></FaCheck>
+                  Intuitive design based on requirements.</Typography>
+              <Typography variant='lead' className='flex gap-5'>
+                <FaCheck className='text-blue text-3xl min-w-10 mt-[.7]'></FaCheck>
+                Designed to be responsive and fit in any device.
+              </Typography>
+              <Typography variant='lead' className='flex gap-5'>
+                <FaCheck className='text-blue text-3xl min-w-10 mt-[.7]'></FaCheck>
+                Fast and precise progressive development.
+              </Typography>
+              <Typography variant='lead' className='flex gap-5'>
+                <FaCheck className='text-blue text-3xl min-w-10 mt-[.7]'></FaCheck>
+                Thorough testing.
+              </Typography>
+              <Typography variant='lead' className='flex gap-5'>
+                <FaCheck className='text-blue text-3xl min-w-10 mt-[.7]'></FaCheck>
+                Deployment.
+              </Typography>
+              <Typography variant='lead' className='flex gap-5'>
+                <FaCheck className='text-blue text-3xl min-w-10 mt-[.7]'></FaCheck>
+                Seamless result.
+              </Typography>
+              </div>
+              <Typography variant='lead' className='font-light mt-10 break-words'>We prioritize quality over anything else. Looking forward working with you.</Typography> 
+            </div>
+            <div className='w-full mt-10'>
+              <Typography variant='h2' className='mb-5'>Products.</Typography>
+              <Typography variant='h4' color='white' className='w-full md:w-96 font-light mb-10'>Currently on progress...</Typography>
             </div>
           </div>
         </section>
-        {/* <section className='w-full h-[86vh] py-[5%] bg-lightGray'>
-          <h1 className='text-center text-2xl md:text-4xl font-bold text-white'>Developers</h1>
-        </section> */}
-        <section className='w-full py-[5%] px-[10%]'>
-          {/* <h1 className='text-center text-2xl md:text-4xl font-bold text-lightGray'></h1> */}
+        <section className='w-full py-[5%]' id='contact'>
           <div className='flex flex-col lg1:flex-row w-full m-auto shadow-xl container mx-auto px-4'>
               <div className='w-full lg1:w-[60%] py-7 text-black'>
-                  <Typography variant='h4' color='blue-gray' className='mb-10'>Get in touch</Typography>
+                  <Typography variant='h4' color='blue-gray' className='mb-5'>Get in touch</Typography>
+                  <Typography color='blue-gray'>Send an email without leaving this page using this form.</Typography>
                   <div className='my-2 w-[40%]'>
                     <Input type='text' label='Name' variant='standard' className='w-full'></Input>
                   </div>
@@ -127,10 +155,16 @@ function App() {
                   <div className='my-2 w-[80%]'>
                     <Textarea label='Message' variant='standard' className='w-full h-32 resize-none'></Textarea>
                   </div>
-                  <Button variant='gradient' className='bg-blue mb-5'>Submit</Button>
+                  <Button variant='gradient' className='bg-blue mb-5 rounded-none'>Submit</Button>
               </div>
-              <div className='w-full lg1:w-[40%] py-7 px-5 rounded bg-blue'>
-                <Typography variant='h4' className='text-white'>Contact us</Typography>
+              <div className='w-full lg1:w-[40%] py-7 px-5 rounded bg-blue break-all'>
+                <Typography variant='h4' className='text-white mb-2'>Contact us</Typography>
+                <Typography variant='lead' className='font-light mb-5'>Keep connected to us!</Typography>
+                <div className='flex flex-col gap-5 my-2'>
+                <Typography variant='lead' className='flex flex-row gap-2'><FaPhoneAlt className='text-2xl min-w-10 mt-1'></FaPhoneAlt>Phone: +63 994 526 6314</Typography>
+                <Typography variant='lead' className='flex flex-row gap-2'><SiGmail className='text-2xl min-w-10 mt-1'></SiGmail>Email: xyronuysoftware@gmail.com</Typography>
+                <Typography variant='lead' className='flex flex-row gap-2'><FaLinkedin className='text-2xl min-w-10 mt-1'></FaLinkedin>LinkedIn: Xyron Uy</Typography>
+                </div>
               </div>
           </div>
         </section>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,6 @@ import {
 
 
 const Header = () => {
-  const [toggleMenu, setToggleMenu] = useState(false)
   const [openNav, setOpenNav] = React.useState(false);
  
   React.useEffect(() => {
@@ -27,38 +26,47 @@ const Header = () => {
     <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Typography
         as="li"
-        variant="small"
+        variant="large"
         className="p-1 font-normal"
       >
         <a href="#" className="flex items-center">
-          Pages
+          Home
         </a>
       </Typography>
       <Typography
         as="li"
-        variant="small"
+        variant="large"
         className="p-1 font-normal"
       >
-        <a href="#" className="flex items-center">
-          Account
+        <a href="#about" className="flex items-center">
+          About
         </a>
       </Typography>
       <Typography
         as="li"
-        variant="small"
+        variant="large"
         className="p-1 font-normal"
       >
-        <a href="#" className="flex items-center">
-          Blocks
+        <a href="#works" className="flex items-center">
+          Works
         </a>
       </Typography>
       <Typography
         as="li"
-        variant="small"
+        variant="large"
         className="p-1 font-normal"
       >
-        <a href="#" className="flex items-center">
-          Docs
+        <a href="#services" className="flex items-center">
+          Services
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="large"
+        className="p-1 font-normal"
+      >
+        <a href="#contact" className="flex items-center">
+          Contact
         </a>
       </Typography>
     </ul>
@@ -124,26 +132,6 @@ const Header = () => {
         </div>
       
       </Navbar>
-    {/* <nav className='min-h-[10vh]'>
-      <div className='h-full container mx-auto flex justify-evenly bg-black sticky top-0 z-50'>
-      <div className='w-[40%] my-auto text-center xl:text-start'>
-          <h1 className='text-2xl md:text-[2.5rem] font-bold italic text-lightColor'>XU Software</h1>
-        </div>
-        <div className='lg:hidden w-[40%] flex justify-end items-center'>
-          <RiMenuFoldLine onClick={() => setToggleMenu(!toggleMenu)} className='text-4xl md:text-5xl text-white'/>
-        </div>
-        <div className={`w-full lg:w-auto my-auto lg:inline left-0 top-[10vh] lg:top-0 absolute lg:relative ${toggleMenu ? "inline bg-black bg-opacity-50 backdrop-blur-sm" : "hidden"}`}>
-          <ul className={`flex lg:justify-end lg:flex-row font-bold text-white text-xl cursor-pointer flex-col`} onClick={() => setToggleMenu(!toggleMenu)}>
-            <li className='m-3 p-1 rounded lg:hover:scale-110 hover:text-[#535bf2]'><a href='#about'>About</a></li>
-            <li className='m-3 p-1 rounded lg:hover:scale-110 hover:text-[#535bf2]'><a>Projects</a></li>
-            <li className='m-3 p-1 rounded lg:hover:scale-110 hover:text-[#535bf2]'><a>Products</a></li>
-            <li className='m-3 p-1 rounded lg:hover:scale-110 hover:text-[#535bf2]'><a>Services</a></li>
-            <li className='m-3 p-1 rounded lg:hover:scale-110 hover:text-[#535bf2]'><a>Developers</a></li>
-            <li className='m-3 p-1 rounded lg:hover:scale-110 hover:text-[#535bf2]'><a>Contact</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav> */}
     </>
   )
 }
