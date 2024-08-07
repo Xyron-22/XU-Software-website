29265d2b52d44e73c759957434910690a2fe2178
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2024-08-07 22:22:40 +0800

    added logo and icon

--- src/components/ClientsSection.jsx
+++ src/components/ClientsSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Typography } from '@material-tailwind/react'
+import westernBrothersLogo from "../assets/westernBrothersLogo.jpg"
 import sampleLogo from "../assets/sample-logo.jpg"
 
 const ClientsSection = () => {
@@ -7,8 +8,8 @@ const ClientsSection = () => {
     <section className='w-full py-5 bg-geoBackground bg-center bg-no-repeat bg-cover bg-blend-darken my-2'>
     <div className='container mx-auto px-4 mb-5'>
       <Typography variant='h4' className='mb-5 text-lightColor text-center'>We've Worked With.</Typography>
-      <div className='w-full gap-2 flex flex-col items-center'>
-        <img src={sampleLogo} alt='company-image' className='w-[40%] md:w-[10%] overflow-hidden'></img>
+      <div className='w-full flex flex-wrap gap-4 justify-center'>
+        <img src={westernBrothersLogo} alt='westernBrothersInc-logo' className='w-28 overflow-hidden rounded'></img>
       </div>
     </div>
   </section>
